5af02d2dbe6aed40a742d98bbd99f07cfdfa4d8b
MJ-Dev <majidbabaeifar@gmail.com>
2022-08-07 10:36:56 +0430

    implemented navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="navbar-brand">Menu</div>
+        <router-link class="nav-item nav-link" to="/">Home</router-link> |
+        <router-link class="nav-item nav-link" to="/list">List</router-link> |
+        <router-link class="nav-item nav-link" to="/counter">Counter</router-link> |
+        <router-link class="nav-item nav-link" to="/conditinal">Conditinal</router-link> |
+        <router-link class="nav-item nav-link" to="/about">About</router-link> |
+        <router-link class="nav-item nav-link" to="/contactus">Contact us</router-link>
+    </nav>
+</template>
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+<style>
+</style>
